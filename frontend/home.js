063889b339c5998aa27d9148e28093d33bf1d7fe
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-01-14 13:42:48 +0800

    NO-TICKET - Initial implementation

---
+++ frontend/home.js
@@ -0,0 +1,10 @@
+const home={template:`
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Hello! Welcome to our Employee Management Portal</h1>
+        <p class="lead">
+            This is a simple employee application, made by Vuejs CDN and Django Rest-API Framework && Poetry Package Management,
+            This allow you to (GET, CREATE, EDIT, DELETE) your details.
+        </p>
+        <hr class="my-4">
+    </div>
+`}
